edd8474976ccd08d2ed0735c8d53b915f7a3adc1
ryanthornton628 <ryanthornton628@gmail.com>
2019-07-09 22:01:19 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,4 +3,5 @@ second_number = 20
 sum = first_number+second_number
 difference = first_number-second_number
 product = first_number*second_number
+quotient = first_number/second_number
 # Add your variables here
